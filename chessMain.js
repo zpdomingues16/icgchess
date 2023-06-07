93b69e0246648ff8d688e334db6184cea096497e
jd <jdomingues@ua.pt>
2023-06-07 19:33:12 +0100

    path solved

--- chessMain.js
+++ chessMain.js
@@ -1,4 +1,4 @@
-import * as THREE from '/js/three.module.js';
+import * as THREE from './js/three.module.js';
 import { OrbitControls } from "./js/OrbitControls.js";
 import { GLTFLoader } from "./js/GLTFLoader.js";
 
